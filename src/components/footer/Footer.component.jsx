ad9158c3a9657bcb2671e616a9311873266d6b53
Arthur Sakemi <arthursakemi@gmail.com>
2020-11-17 00:29:28 -0300

    footer component removed, replaced by Grommet Footer

--- src/components/footer/Footer.component.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import { StyledFooter } from './footer.styles';
-
-const Footer = () => {
-  return <StyledFooter>Created by Arthur Sakemi 2020</StyledFooter>;
-};
-
-export default Footer;
