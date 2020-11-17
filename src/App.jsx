28e729b629b49fc2ad2a5d350b11fbf4435ba10b
Arthur Sakemi <arthursakemi@gmail.com>
2020-11-17 00:29:01 -0300

    antdesign remove, and Grommet added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import GlobalStyle from './styles/global.styles';
 
+import { grommet, Grommet } from 'grommet';
+
 import HomePage from './pages/home/home.page';
+import { deepMerge } from 'grommet/utils';
+
+const theme = {
+  global: {
+    colors: {
+      background: {
+        light: '#F5F5F5',
+        dark: '#282a36',
+      },
+    },
+  },
+};
+
+const customTheme = deepMerge(grommet, theme);
 
 function App() {
   return (
     <div className='App'>
-      <GlobalStyle />
-      <HomePage />
+      <Grommet theme={customTheme} themeMode='dark' full>
+        <GlobalStyle />
+        <HomePage />
+      </Grommet>
     </div>
   );
 }
