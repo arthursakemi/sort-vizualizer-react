4d587246efafff1d1253feba3ce605c316ac8956
Arthur Sakemi <arthursakemi@gmail.com>
2020-11-17 17:29:58 -0300

    bubble sort refactor

--- src/sortingAlgorithms/bubbleSort.js
+++ src/sortingAlgorithms/bubbleSort.js
@@ -3,21 +3,27 @@ import { delay } from '../helperFunctions/delay';
  * @param {Number[]} originalArray the array to be sorted
  * @param {Function} setArray setter for the array state
  */
-async function bubbleSort(originalArray, setArray) {
+
+export default async function bubbleSort(originalArray, setArray) {
   let array = [...originalArray];
 
   for (let i = 1; i < array.length; i++) {
     for (let j = 0; j < array.length - i; j++) {
-      if (array[j + 1] < array[j]) {
-        let temp = array[j + 1];
-        array[j + 1] = array[j];
-        array[j] = temp;
-        await delay(1);
-        setArray(array);
-        array = [...array];
-      }
+      await compareAndSwap(array, j, setArray);
     }
   }
 }
 
-export default bubbleSort;
+async function compareAndSwap(array, j, setArray) {
+  if (array[j + 1] < array[j]) {
+    swap(array, j);
+    await delay(1);
+    setArray([...array]);
+  }
+}
+
+function swap(array, j) {
+  let temp = array[j + 1];
+  array[j + 1] = array[j];
+  array[j] = temp;
+}
